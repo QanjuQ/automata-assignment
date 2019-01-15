986e34e7d1a5d912f850536b308329caa574016f
Anjum Qureshi <anjumqureshi000@gmail.com>
2019-01-15 10:38:32 +0530

    run tests

---
+++ test_runner.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const DFA = require('./dfa');
+const NFA = require('./nfa');
+
+const tests = require('./tests.js')
+// .filter(machine => machine.type === 'nfa');
+
+const createMachine = (type, tuple) => {
+  const machine = { dfa: DFA, nfa: NFA };
+  return new machine[type](tuple);
+};
+
+const runtest = (machineInfo) => {
+  console.log(`\n********${machineInfo.type}: ${machineInfo.name}********`);
+  machineInfo['pass-cases'].forEach((input) => {
+    const machine = createMachine(machineInfo.type, machineInfo.tuple);
+    console.log(`Should return true for ${input}`); ``
+    assert(machine.doesAccept(input));
+  });
+  machineInfo['fail-cases'].forEach((input) => {
+    const machine = createMachine(machineInfo.type, machineInfo.tuple);
+    console.log(`Should return false for ${input}`);
+    assert(!machine.doesAccept(input));
+  });
+};
+
+tests.forEach(runtest);
